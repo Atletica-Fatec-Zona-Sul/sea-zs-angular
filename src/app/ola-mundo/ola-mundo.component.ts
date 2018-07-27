bba0e9063886f312dece4bd120ebcbe8a8e5b0fa
Daniel Soares <daniel.soares@zup.com.br>
2018-07-27 18:42:33 -0300

    Setup Inicial com um Hello World

---
+++ src/app/ola-mundo/ola-mundo.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "../../../node_modules/@angular/core";
+
+
+@Component({
+    selector: 'ola-mundo',
+    templateUrl: './ola-mundo.component.html',
+    styleUrls:  ['./ola-mundo.component.css']
+})
+export class OlaMundoComponent {
+
+}
