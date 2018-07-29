89f6ab4a609ef36522099c074a9f4ce3ffd5b488
Daniel Soares <daniel.soares@zup.com.br>
2018-07-29 12:39:59 -0300

    Painel eventos iterando array

--- src/app/painel-eventos/painel-eventos.component.ts
+++ src/app/painel-eventos/painel-eventos.component.ts
@@ -9,6 +9,35 @@ export class PainelEventosComponent implements OnInit {
 
   constructor() { }
 
+  eventos = [{
+        "id": 1,
+    "nome": "Semana de Técnologia",
+    "descricao": "Serão feitas diversas atividades com os alunos da Fatec Zona Sul, entre elas palestras, workshops e treinamentos.",
+    "dataInicio": "06/09/2018",
+    "dataFim": "11/09/2018"
+  },
+  {
+    "id": 2,
+    "nome": "Atlética Fatec Zona Sul",
+    "descricao": "Atividades esportivas e campeonatos para os alunos da Fatec Zona Sul participarem durante o semestre",
+    "dataInicio": "01/08/2018",
+    "dataFim": "10/12/2018"
+  },
+  {
+    "id": 3,
+    "nome": "Atlética Fatec Zona Sul",
+    "descricao": "Atividades esportivas e campeonatos para os alunos da Fatec Zona Sul participarem durante o semestre",
+    "dataInicio": "01/08/2018",
+    "dataFim": "10/12/2018"
+  },
+  {
+    "id": 4,
+    "nome": "Atlética Fatec Zona Sul",
+    "descricao": "Atividades esportivas e campeonatos para os alunos da Fatec Zona Sul participarem durante o semestre",
+    "dataInicio": "01/08/2018",
+    "dataFim": "10/12/2018"
+  }]
+
   ngOnInit() {
   }
 
