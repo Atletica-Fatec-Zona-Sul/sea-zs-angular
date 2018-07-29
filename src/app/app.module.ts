d9cbcd207e7122994745b73ab42f7f664903ca29
Daniel Soares <daniel.soares@zup.com.br>
2018-07-29 11:25:16 -0300

    Acertado so imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { PainelEventosComponent } from './painel-eventos/painel-eventos.component';
-import { MenuLateralComponent } from './barra-navegacao/menu-lateral/menu-lateral.component';
 import { BarraNavegacaoComponent } from './barra-navegacao/barra-navegacao.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PainelEventosComponent,
-    MenuLateralComponent,
     BarraNavegacaoComponent
   ],
   imports: [
