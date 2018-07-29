f7f94864e326d27ca97ea8b09ea100a2570bc48d
Daniel Soares <daniel.soares@zup.com.br>
2018-07-29 17:02:56 -0300

    Agendamento continuando

--- src/app/eventos/consulta-evento/consulta-evento.component.ts
+++ src/app/eventos/consulta-evento/consulta-evento.component.ts
@@ -9,7 +9,40 @@ import { ActivatedRoute } from '@angular/router';
 export class ConsultaEventoComponent implements OnInit {
 
   id: number;
-
+  agendamento = {
+    "id":1,
+    "inicio": "06/07/2018 - 07:00",
+    "fim": "06/07/2018 - 12:00",
+    "usuarioOrganizador": "Daniel Soares",
+    "atividades": [{
+      "id": 1,
+      "nome": "Inovação no século XXI",
+      "descricao": "A inovação no século XXI é muito importante por que bla bla bla bla bla bla bla bla bla bla bla bla",
+      "categoriaAtividade": "Palestra",
+      "localAtividade": "Auditório",
+      "inicioAtividade": "06/07/2018 - 07:00",
+      "fimAtividade":"06/07/2018 - 09:00",
+      "cargaHoraria": 3,
+      "qtdVagas": 60,
+      "pontuacaoParticipante": 60,
+      "flagContinua": false,
+      "flagCumulativa": true
+    },
+    {
+      "id": 2,
+      "nome": "Inovação no século XXI",
+      "descricao": "A inovação no século XXI é muito importante por que bla bla bla bla bla bla bla bla bla bla bla bla",
+      "categoriaAtividade": "Palestra",
+      "localAtividade": "Auditório",
+      "inicioAtividade": "06/07/2018 - 07:00",
+      "fimAtividade":"06/07/2018 - 09:00",
+      "cargaHoraria": 3,
+      "qtdVagas": 60,
+      "pontuacaoParticipante": 60,
+      "flagContinua": false,
+      "flagCumulativa": true
+    }]
+  };
   constructor(private route : ActivatedRoute) { }
 
   ngOnInit() {
